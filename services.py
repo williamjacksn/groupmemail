c8ddaa5b6f05476358778313f3f2e7923d257f29
William Jackson <william@subtlecoolness.com>
2013-11-19 10:33:59 -0600

    Send email

--- services.py
+++ services.py
@@ -1,12 +1,31 @@
+import os
+import requests
+
 from flask import Flask, request
+from postmark import PMMail
+
 app = Flask(__name__)
 
-@app.route(u'/groupme/famablama', methods=[u'POST'])
+POSTMARK_API_KEY = os.environ.get(u'POSTMARK_API_KEY')
+GROUPME_ACCESS_TOKEN = os.environ.get(u'GROUPME_ACCESS_TOKEN')
+
+@app.route(u'/groupme/new_message', methods=[u'POST'])
 def hello_word():
     j = request.get_json()
-    if u'name' in j and u'text' in j:
-        print(u'{name} said: {text}'.format(**j))
+    g = requests.get(
+        u'https://api.groupme.com/v3/groups/{group}'.format(**j),
+        params={u'token': GROUPME_ACCESS_TOKEN}
+    )
+    group_name = g.json().get(u'response').get(u'name')
+    m = PMMail(
+        api_key=POSTMARK_API_KEY,
+        subject=u'New message in {}'.format(group_name),
+        sender=u'william@subtlecoolness.com',
+        to=u'william@subtlecoolness.com',
+        text_body=u'{name} said: {text}'.format(**j)
+    )
+    m.send(test=False)
     return u'Thank you.'
 
 if __name__ == u'__main__':
-    app.run(debug=True, host=u'0.0.0.0')
+    app.run(host=u'0.0.0.0')
