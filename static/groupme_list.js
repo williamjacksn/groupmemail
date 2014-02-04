6dc6450636bed71d9d9a760b970cd3ca94039652
William Jackson <william@subtlecoolness.com>
2014-02-03 20:56:35 -0600

    Rework web interface

--- static/groupme_list.js
+++ static/groupme_list.js
@@ -1,10 +1,15 @@
-var GROUPME_AUTH_TOKEN = "";
-var metas = document.getElementsByTagName("meta");
-
-for (i = 0; i < metas.length; i++) {
-    if (metas.item(i).getAttribute("name") == "groupme_auth_token") {
-        GROUPME_AUTH_TOKEN = metas.item(i).getAttribute("content");
+var cookies = document.cookie.split("; ");
+for (i = 0; i < cookies.length; i++) {
+    c = cookies[i].split("=");
+    if (c[0] = "groupme_auth_token") {
+        var groupme_auth_token = c[1]
     }
+}
+
+function add_class(element, class_name) {
+    var current_classes = element.className.split(" ");
+    current_classes.push(class_name);
+    element.className = current_classes.join(" ");
 }
 
 function groupme_users_me_callback() {
@@ -17,7 +22,8 @@ function groupme_users_me_callback() {
     var text = "Hello, " + resp.name + ".";
     var name_txt = document.createTextNode(text);
 
-    h1_el = document.getElementsByTagName("h1").item(0);
+    var h1_el = document.getElementsByTagName("h1").item(0);
+    h1_el.setAttribute("id", resp.user_id);
     h1_el.textContent = "";
     h1_el.appendChild(img_el);
     h1_el.appendChild(name_txt);
@@ -35,42 +41,37 @@ function groupme_groups_callback() {
     for (i = 0; i < resp.length; i++) {
         var group = resp[i];
         var li_el = document.createElement("li");
+        li_el.setAttribute("id", group.group_id);
         li_el.innerText = group.name;
         group_list.appendChild(li_el);
     }
+
+    var xhr_bots = new XMLHttpRequest();
+    xhr_bots.onload = groupme_bots_callback;
+    var url = "https://api.groupme.com/v3/bots?token=" + groupme_auth_token;
+    xhr_bots.open("get", url);
+    xhr_bots.send();
 }
 
 function groupme_bots_callback() {
     var resp = JSON.parse(this.responseText).response;
-    var bot_p = document.getElementById("bot_p");
-    if (resp.length == 0) {
-        bot_p.innerText = "No bots found.";
-    } else {
-        bot_p.innerText = "Here are your bots:";
-    }
-    var bot_list = document.getElementById("bot_list");
     for (i = 0; i < resp.length; i++) {
         var bot = resp[i];
-        var li_el = document.createElement("li");
-        li_el.innerText = bot.name + " on " + bot.group_name;
-        bot_list.appendChild(li_el)
+        if (bot.callback_url.indexOf("/groupme/incoming/") > -1) {
+            var group_li = document.getElementById(bot.group_id);
+            add_class(group_li, "subscribed");
+        }
     }
 }
 
 var xhr_users_me = new XMLHttpRequest();
 xhr_users_me.onload = groupme_users_me_callback;
-var url = "https://api.groupme.com/v3/users/me?token=" + GROUPME_AUTH_TOKEN;
+var url = "https://api.groupme.com/v3/users/me?token=" + groupme_auth_token;
 xhr_users_me.open("get", url);
 xhr_users_me.send();
 
 var xhr_groups = new XMLHttpRequest();
 xhr_groups.onload = groupme_groups_callback;
-url = "https://api.groupme.com/v3/groups?token=" + GROUPME_AUTH_TOKEN;
+url = "https://api.groupme.com/v3/groups?token=" + groupme_auth_token;
 xhr_groups.open("get", url);
 xhr_groups.send();
-
-var xhr_bots = new XMLHttpRequest();
-xhr_bots.onload = groupme_bots_callback;
-url = "https://api.groupme.com/v3/bots?token=" + GROUPME_AUTH_TOKEN;
-xhr_bots.open("get", url);
-xhr_bots.send();
