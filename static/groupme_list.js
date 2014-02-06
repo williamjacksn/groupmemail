ee664a976f9c2ab0818a1c2186f4e90fca0ad9b2
William Jackson <william@subtlecoolness.com>
2014-02-06 12:47:33 -0600

    Use Bootstrap

--- static/groupme_list.js
+++ static/groupme_list.js
@@ -12,6 +12,17 @@ function add_class(element, class_name) {
     element.className = current_classes.join(" ").trim();
 }
 
+function drop_class(element, class_name) {
+    var current_classes = element.className.split(" ");
+    var resulting_classes = [];
+    for (i = 0; i < current_classes.length; i++) {
+        if (current_classes[i] != class_name) {
+            resulting_classes.push(current_classes[i]);
+        }
+    }
+    element.className = resulting_classes.join(" ").trim();
+}
+
 function groupme_users_me_callback() {
     var resp = JSON.parse(this.responseText).response;
     var img_src = resp.image_url.replace("http:", "https:");
@@ -19,14 +30,7 @@ function groupme_users_me_callback() {
     img_el.setAttribute("src", img_src);
     img_el.setAttribute("width", "40");
 
-    var text = "Hello, " + resp.name + ".";
-    var name_txt = document.createTextNode(text);
-
-    var h1_el = document.getElementsByTagName("h1").item(0);
-    h1_el.setAttribute("id", resp.user_id);
-    h1_el.textContent = "";
-    h1_el.appendChild(img_el);
-    h1_el.appendChild(name_txt);
+    document.getElementById("groupme_username").textContent = resp.name;
 }
 
 function groupme_groups_callback() {
@@ -35,15 +39,24 @@ function groupme_groups_callback() {
     if (resp.length == 0) {
         group_p.innerText = "No groups found.";
     } else {
-        group_p.innerText = "Here are your groups:";
+        group_p.innerText = "Click on a group to toggle email notifications.";
     }
     var group_list = document.getElementById("group_list");
     for (i = 0; i < resp.length; i++) {
         var group = resp[i];
-        var li_el = document.createElement("li");
-        li_el.setAttribute("id", group.group_id);
-        li_el.innerText = group.name;
-        group_list.appendChild(li_el);
+
+        var a_el = document.createElement("a");
+        a_el.setAttribute("id", group.group_id);
+        a_el.setAttribute("href", "#");
+        a_el.innerText = group.name;
+        add_class(a_el, "list-group-item");
+
+        var new_span = document.createElement("span");
+        new_span.setAttribute("id", group.group_id + "_badge")
+        add_class(new_span, "badge");
+        new_span.innerText = "Not subscribed ✗";
+        a_el.appendChild(new_span);
+        group_list.appendChild(a_el);
     }
 
     var xhr_bots = new XMLHttpRequest();
@@ -58,8 +71,10 @@ function groupme_bots_callback() {
     for (i = 0; i < resp.length; i++) {
         var bot = resp[i];
         if (bot.callback_url.indexOf("/groupme/incoming/") > -1) {
-            var group_li = document.getElementById(bot.group_id);
-            add_class(group_li, "subscribed");
+            var group_el = document.getElementById(bot.group_id);
+            var span = group_el.lastChild;
+            span.innerText = "Subscribed ✓";
+            add_class(group_el, "list-group-item-success");
         }
     }
 }
