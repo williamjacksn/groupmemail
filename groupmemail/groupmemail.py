1dc881735bd917ac510d842e7343fdd14792b274
William Jackson <william@subtlecoolness.com>
2018-11-12 14:45:24 -0600

    Change request log back to DEBUG

--- groupmemail/groupmemail.py
+++ groupmemail/groupmemail.py
@@ -64,7 +64,7 @@ def external_url(endpoint, **values):
 
 @app.before_request
 def log_request():
-    app.logger.info(f'{flask.request.method} {flask.request.path}')
+    app.logger.debug(f'{flask.request.method} {flask.request.path}')
 
 
 @app.route('/ping')
