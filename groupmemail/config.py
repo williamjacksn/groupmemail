21b884c86adea6764aff470dc2b048735f2c891e
William Jackson <565174+williamjacksn@users.noreply.github.com>
2023-05-25 13:23:08 -0500

    Remove a bunch of config

--- groupmemail/config.py
+++ groupmemail/config.py
@@ -2,25 +2,17 @@ import os
 
 
 class Config:
-    groupme_client_id: str
     log_format: str
     log_level: str
-    mailgun_api_key: str
-    mailgun_domain: str
     scheme: str
     secret_key: str
     server_name: str
-    stripe_publishable_key: str
-    stripe_secret_key: str
-    stripe_sku: str
-    stripe_webhook_secret: str
     version: str
 
     def __init__(self):
         """Instantiating a Config object will automatically read the following environment variables:
 
-        APP_VERSION, GROUPME_CLIENT_ID, LOG_FORMAT, LOG_LEVEL, MAILGUN_API_KEY, MAILGUN_DOMAIN, SCHEME, SECRET_KEY,
-        SERVER_NAME, STRIPE_PUBLISHABLE_KEY, STRIPE_SECRET_KEY, STRIPE_SKU, STRIPE_WEBHOOK_SECRET
+        APP_VERSION, LOG_FORMAT, LOG_LEVEL, SCHEME, SECRET_KEY, SERVER_NAME
 
         Some variables have defaults if they are not found in the environment:
 
@@ -28,16 +20,9 @@ class Config:
         LOG_LEVEL=INFO
         SCHEME=http"""
 
-        self.groupme_client_id = os.getenv('GROUPME_CLIENT_ID')
         self.log_format = os.getenv('LOG_FORMAT', '%(levelname)s [%(name)s] %(message)s')
         self.log_level = os.getenv('LOG_LEVEL', 'INFO')
-        self.mailgun_api_key = os.getenv('MAILGUN_API_KEY')
-        self.mailgun_domain = os.getenv('MAILGUN_DOMAIN')
         self.scheme = os.getenv('SCHEME', 'http')
         self.secret_key = os.getenv('SECRET_KEY')
         self.server_name = os.getenv('SERVER_NAME')
-        self.stripe_publishable_key = os.getenv('STRIPE_PUBLISHABLE_KEY')
-        self.stripe_secret_key = os.getenv('STRIPE_SECRET_KEY')
-        self.stripe_sku = os.getenv('STRIPE_SKU')
-        self.stripe_webhook_secret = os.getenv('STRIPE_WEBHOOK_SECRET')
         self.version = os.getenv('APP_VERSION', 'unknown')
